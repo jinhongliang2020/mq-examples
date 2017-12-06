a790c59d59966c4f5a190f15ad0ebbf2ebf9b423
hong <1043518275@qq.com>
2017-12-06 10:11:37 +0800

    kafka 整合spring 发送消息.

---
+++ kafka/kafka-producer/src/main/java/org/hong/kafka/constant/KafkaMesConstant.java
@@ -0,0 +1,20 @@
+package org.hong.kafka.constant;
+
+/**
+ * @Description: (kafkaMessageConstant)
+ * @author hong
+ * @date 2017/11/23
+ * @version v1.1
+ */
+public class KafkaMesConstant {
+
+	public static final String SUCCESS_MES = "===============成功";
+
+	/**
+	 * kakfa-msg
+	 */
+	public static final String KAFKA_SEND_ERROR_MES = "发送消息超时,联系相关技术人员";
+	public static final String KAFKA_NO_RESULT_MES = "未查询到返回结果,联系相关技术人员";
+	public static final String KAFKA_NO_OFFSET_MES = "未查到返回数据的offset,联系相关技术人员";
+
+}
